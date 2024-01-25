2cc88c836ff249ebb7bab4ba6b6b65d7e0956526
jonatansteiger <121043640+jonatansteiger@users.noreply.github.com>
2024-01-25 15:23:20 -0300

    desafio03.js

--- desafio03.js
+++ desafio03.js
@@ -1,1 +1,17 @@
+class Heroi{
+    constructor(nome, idade, tipo, arma){
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+        this.arma = arma;
+    }
 
+    ataqueDoHeroi(){
+        console.log(`O ${this.tipo} chamado ${this.nome} de ${this.idade} anos, atacou usando ${this.arma}!`);
+    }
+}
+
+
+let heroi1 = new Heroi("Maltazar", 68, "mago negro", "magia negra");
+
+heroi1.ataqueDoHeroi();
